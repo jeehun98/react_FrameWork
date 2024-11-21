8d37d86726bcab638afb097c3d06ea86cc1e714e
jhkim <as04263@gmail.com>
2024-11-22 08:13:36 +0900

    컴포넌트 조합 기본

---
+++ src/MyComponent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function MyComponent() {
+  return (
+    <div>
+      <h2>Hello from MyComponent!</h2>
+      <p>This is a reusable component.</p>
+    </div>
+  );
+}
+
+export default MyComponent;
